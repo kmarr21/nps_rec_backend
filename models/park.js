905747b94d2b73221cb62a4ba50582245afbe0c6
Abby <abbyray08@gmail.com>
2025-08-04 16:11:53 -0400

    Updates to mongoose schema for all parksData

--- models/park.js
+++ models/park.js
@@ -3,8 +3,19 @@ const mongoose = require('mongoose');
 
 //mongoose parks data schema
 //a schema of the data we will use from the database
-const parkSchema = new mongoose.Schema({
-    parkCode: String
-});
+const parkSchema = new mongoose.Schema(
+    {
+        fullName: String,
+        activities: Array,
+        parkCode: String,
+        visitation: Array,
+        description: String,
+        images: Array,
+        weather: Object
+
+    },
+    {
+        collection: 'parksAllData'
+    });
 
 module.exports = mongoose.model('Park', parkSchema);
