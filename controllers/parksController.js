d4a46826442acffcb3e4333e0b5c92b40c470eb9
Abby <abbyray08@gmail.com>
2025-08-04 16:57:43 -0400

    updating fields returned for five parks run

--- controllers/parksController.js
+++ controllers/parksController.js
@@ -6,8 +6,7 @@ exports.getFiveParks = async (req, res) => {
     try {
         console.log('getFiveParks controller hit');
         mongoose.set('debug', true);
-        const parks = await Park.find().limit(5);
-        console.log('Parks found:', parks);
+        const parks = await Park.find().limit(5).select('-activitites -description -fullName -images -parkCode -visitation -weather');
         res.json(parks);
     } catch (error) {
         console.error("Error fetching parks: ", error);
