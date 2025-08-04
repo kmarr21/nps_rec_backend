86a4f36e529081a609ecc269f285ab033cf239f5
Abby <abbyray08@gmail.com>
2025-08-04 18:09:26 -0400

    add latitude longitude

--- controllers/parksController.js
+++ controllers/parksController.js
@@ -6,7 +6,7 @@ exports.getFiveParks = async (req, res) => {
     try {
         console.log('getFiveParks controller hit');
         mongoose.set('debug', true);
-        const parks = await Park.find().limit(5).select('activitites description fullName images parkCode topics visitation weather');
+        const parks = await Park.find().limit(10).select('activitites description fullName images latitude longitude parkCode topics visitation weather');
         res.json(parks);
     } catch (error) {
         console.error("Error fetching parks: ", error);
