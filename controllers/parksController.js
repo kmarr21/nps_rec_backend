f1dc7d4c35ae2f781b1788450c00ecd5354cfc81
Abby <abbyray08@gmail.com>
2025-08-04 16:26:18 -0400

    test parksController

--- controllers/parksController.js
+++ controllers/parksController.js
@@ -3,14 +3,9 @@ const mongoose = require('mongoose');
 
 //Get park data
 exports.getFiveParks = async (req, res) => {
-    try {
-        console.log('getFiveParks controller hit');
-        mongoose.set('debug', true);
-        const parks = await Park.find().limit(5);
-        console.log('Parks found:', parks);
-        res.json(parks);
-    } catch (error) {
-        console.error("Error fetching parks: ", error);
-        res.status(500).json({ error: 'failed to fetch five parks' });
-    }
+    console.log('getFiveParks controller hit');
+    res.json([
+        { parkCode: 'YNP', fullName: 'Yellowstone National Park' },
+        { parkCode: 'GRCA', fullName: 'Grand Canyon National Park' }
+    ]);
 };
