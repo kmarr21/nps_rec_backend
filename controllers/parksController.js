a34a9d1cfe2c294d4648f810ba3fe965daea3e2d
Abby <abbyray08@gmail.com>
2025-08-04 16:50:17 -0400

    more mongo debugging

--- controllers/parksController.js
+++ controllers/parksController.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose');
 
 //Get park data
 exports.getFiveParks = async (req, res) => {
-    console.log('getFiveParks controller hit');
-    exports.getFiveParks = async (req, res) => {
-        try {
-            console.log('getFiveParks controller hit');
-            const parks = await Park.find().limit(5);
-            console.log('Parks found:', parks);
-            res.json(parks);
-        } catch (error) {
-            console.error("Error fetching parks: ", error);
-            res.status(500).json({ error: 'failed to fetch five parks' });
-        }
-    };
+    try {
+        console.log('getFiveParks controller hit');
+        mongoose.set('debug', true);
+        const parks = await Park.find().limit(5);
+        console.log('Parks found:', parks);
+        res.json(parks);
+    } catch (error) {
+        console.error("Error fetching parks: ", error);
+        res.status(500).json({ error: 'failed to fetch five parks' });
+    }
 };
