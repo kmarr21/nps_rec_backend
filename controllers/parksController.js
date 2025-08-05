d03dbf20481f39edbecfe18a62453c96563ff41c
Mitchell Finitz <mfinit02@tufts.edu>
2025-08-05 02:03:12 -0400

    Testing a find query 2

--- controllers/parksController.js
+++ controllers/parksController.js
@@ -6,7 +6,9 @@ exports.getFiveParks = async (req, res) => {
     try {
         console.log('getFiveParks controller hit');
         mongoose.set('debug', true);
-        const parks = await Park.find({'activities.name':'Golf'}).select('activities description fullName');
+        const parks = await Park.find({
+            "activities.name": { $in: ["Cross-Country Skiing","Flying"] }})
+            .select('activities description fullName images latitude longitude parkCode topics visitation weather');
         res.json(parks);
     } catch (error) {
         console.error("Error fetching parks: ", error);
