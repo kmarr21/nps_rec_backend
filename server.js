7e64aeb941761649fc84169fcca8b92918ff8651
Kiersten Marr <kierstenmarr@Kierstens-Laptop.local>
2025-08-06 13:51:52 -0700

    ddd

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
